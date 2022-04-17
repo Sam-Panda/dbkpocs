d8148947b4d2b49e5d6553525ec0686d1228583d
samarendra.panda@outlook.com <samarendra.panda@outlook.com>
2022-04-17 05:58:18 +0000

    .
    .

--- databricks_notebooks/hive external metadata.py
+++ databricks_notebooks/hive external metadata.py
@@ -1,2 +1,21 @@
 # Databricks notebook source
+# MAGIC %scala
+# MAGIC 
+# MAGIC import com.typesafe.config.ConfigFactory
+# MAGIC val path = ConfigFactory.load().getString("java.io.tmpdir")
+# MAGIC 
+# MAGIC println(s"\nHive JARs are downloaded to the path: $path \n")
+
+# COMMAND ----------
+
+# MAGIC %sh cp -r /local_disk0/tmp /dbfs/hive_metastore_jar
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC create database testdb
+
+# COMMAND ----------
+
 
